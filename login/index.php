7191e358c35c235e928992431e9e889b2f1a0ad3
louisclararena <63798481+louisclararena@users.noreply.github.com>
2020-04-16 22:26:31 +0700

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+if (isset($_SESSION["id_petugas"])) {
+    header("Location : http://localhost/siperpus/index.php");
+} 
+?>
+<form action="proses-login.php" method="post">
+    <table>
+        <tr>
+            <td><label for="">Username</label></td>
+            <td><input type="text" name="username" id=""></td>
+        </tr>
+        <tr>
+            <td><label for="">Password</label></td>
+            <td><input type="password" name="password" id=""></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+            <input type="submit" value="Login" name="btnlogin">
+            </td>
+        </tr>
+    </table>
+    </form>
